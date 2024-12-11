5f73cd6a14cd68a441816b7b59a66b86730be10c
garychanchihang <garychan8523@gmail.com>
2024-12-11 17:50:00 +0800

    updates for day11

--- 2024/day11/1_and_2.py
+++ 2024/day11/1_and_2.py
@@ -22,29 +22,29 @@ def get_last_n_digits(num, n):
     return abs(num) % (10**n)
 
 @lru_cache(maxsize=274_877_906_944)
-def blink(num, target, iter=0, count=0):
+def blink(num, target, iter=0):
     # print(f'blink {num} target: {target}  iter: {iter}  count: {count}')
     if iter == target:
-        return count
+        return 1
     
     iter += 1
 
     if num == 0:
-        return blink(1, target, iter, count)
+        return blink(1, target, iter)
     
     digits_length = get_digits_length(num)
     if digits_length % 2 == 0:
         half_length = digits_length // 2
-        left_count = blink(get_first_n_digits(num, half_length), target, iter, count)
-        right_count = blink(get_last_n_digits(num, half_length), target, iter, count)
+        left_count = blink(get_first_n_digits(num, half_length), target, iter)
+        right_count = blink(get_last_n_digits(num, half_length), target, iter)
         return left_count + right_count
     else:
-        return blink(num * 2024, target, iter, count)
+        return blink(num * 2024, target, iter)
 
     
     
 def solver(stone, target):
-    return blink(stone, target, iter=0, count=1)
+    return blink(stone, target, iter=0)
 
 
 
